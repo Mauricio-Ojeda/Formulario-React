c6ae261d6bf6e8bd9b6e18bd0d3aca89908b8eed
Mau <70604758+Mauricio-Ojeda@users.noreply.github.com>
2021-05-03 12:00:27 -0300

    Agregando eventos al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,81 +1,149 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import './styles/formulario.css'
+import { v4 as uuid } from 'uuid';
 
 const Formulario = () => {
+    
+    //crear state de cita
+    const [cita, actualizarCita] = useState({
+
+        mascota: '',
+        especie:'',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '',
+    })
+
+    const [error, actualizarError] = useState(false)
+
+    // handleChange input
+
+    const handleChange = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    //hadle Submit
+    const handleSubmit = e =>{
+        e.preventDefault();
+        
+        // validar
+        if(mascota.trim() === '' | especie.trim() === '' | propietario.trim() === ''
+         | fecha.trim() === '' | hora.trim() === '' | sintomas.trim() === ''){
+            actualizarError(true)
+            return
+        }
+
+        // Eliminar mensaje error
+        actualizarError(false);
+
+        //Asignar ID
+        cita.id = uuid();
+
+
+    }
+
+    // Extraer Valores
+
+    const {mascota, especie, propietario, fecha, hora, sintomas} = cita;
+
+
     return (
         <Fragment>
+
             <h2>Crear Cita</h2>
-            <form>
-                <label for="mascota">Nombre Mascota
+            {error ? <p className="alerta-error"> Todos los Campos deben ser Completados </p> : null}
+
+            <form
+                onSubmit={handleSubmit}    
+            >
+                <label htmlFor="mascota">Nombre Mascota
                 <input
                     id="mascota"
                     type= "text"
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre Mascota"
+                    onChange={handleChange}
+                    value={mascota}
 
                 />                
                 </label>
                 <div className="row-radio">
                     <div className="perro-div">
-                        <label for="perro">Perro
+                        <label htmlFor="perro">Perro
                         <input
                             id="perro"
                             type= "radio"
-                            name="especie"                    
+                            name="especie"
+                            value="perro"
+                            onChange={handleChange}
                             
                         />
                         </label>                  
                     </div>
                     <div className='gato-div'>
-                        <label for="gato">Gato
+                        <label htmlFor="gato">Gato
                         <input
                             id="gato"
                             type= "radio"
-                            name="especie"                
-                            
+                            name="especie"
+                            value="gato"                
+                            onChange={handleChange}
+
                         /> 
                         </label>               
                     </div>
                 </div>
 
-                <label for="propietario">Nombre Dueño
+                <label htmlFor="propietario">Nombre Dueño
                 <input
                     id="propietario"
                     type= "text"
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre Dueño de la mascota"
+                    onChange={handleChange}
+                    value={propietario}
 
                 />
                 </label>
 
-                <label for="fecha">Fecha
+                <label htmlFor="fecha">Fecha
                 <input
                     id="fecha"
                     type= "date"
                     name="fecha"
                     className="u-full-width"             
+                    onChange={handleChange}
+                    value={fecha}
 
                 />
                 </label>
 
-                <label for="hora">Hora
+                <label htmlFor="hora">Hora
                 <input
                     id="hora"
                     type= "time"
                     name="hora"
-                    className="u-full-width"             
+                    className="u-full-width" 
+                    onChange={handleChange} 
+                    value={hora}           
 
                 />
                 </label>
 
-                <label for="sintomas">Sintomas
+                <label htmlFor="sintomas">Sintomas
                 <textarea
                     id="sintomas"
-                    name="sinotmas"
+                    name="sintomas"
                     className="u-full-width"             
                     placeholder="¿Que sintomas tiene tu mascota?"
+                    onChange={handleChange}
+                    value={sintomas}
 
                 ></textarea>
                 </label>
